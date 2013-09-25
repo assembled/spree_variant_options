1794dfb33ac76d6ec7efa538290eb792ea847042
Ag <github@assembledgoods.com>
2013-09-24 20:02:04 -0500

    support spree 2.0.X

--- lib/spree_variant_options.rb
+++ lib/spree_variant_options.rb
@@ -1,4 +1,7 @@
 require "spree_core"
+require "spree_api"
+require "spree_backend"
+require "spree_frontend"
 require "spree_sample" unless Rails.env.production?
 
 require "spree_variant_options/engine"
